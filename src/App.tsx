68096962893391dc5672451d22af8e3f6eec7e83
nikizak34 <nikita2000.16.11@gmail.com>
2023-10-05 22:56:49 +0300

    add switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Redirect, Route, withRouter} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch, withRouter} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsConteiner";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
@@ -32,26 +32,25 @@ class App extends React.Component<HeaderPropsType> {
             return <Preloader/>
         }
         return (
-            <BrowserRouter >
+            <BrowserRouter>
                 <div className="app-wrapper">
                     <HeaderContainer/>
                     <div className="flex-00">
                         <Navbar/>
                         <div className='app-wrapper-content'>
+                            <Switch>
+                                <Route exact path='/' render={() => <Redirect to={'/profile'}/>}/>
+                                <Route path='/dialogs'
+                                       render={() => <DialogsContainer/>}/>
 
-                            <Route path='/dialogs'
-                                   render={() => <DialogsContainer/>}/>
+                                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
 
-                            <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                                <Route path='/users' render={() => <UsersContainer/>}/>
 
-                            <Route path='/users' render={() => <UsersContainer/>}/>
+                                <Route path='/login' render={() => <Login/>}/>
 
-                            <Route path='/login' render={() => <Login/>}/>
-
-                            <Route path='*' render={() =><div>404 NOT FOUND</div>}/>
-
-                            <Route exact path='/' render={() =><Redirect to={'/profile'}/>}/>
-
+                                <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
+                            </Switch>
 
 
                         </div>
